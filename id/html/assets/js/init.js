ba721ca8d8d097a37defac5919a9cd011530c997
theflyingjets <theflyingjets@users.noreply.github.com>
2019-05-20 13:18:46 -0400

    ID:V0.01
    I cleaned up the code and also fixxed the  SQL code due to a missing * in the select * form "tabel name"

--- id/html/assets/js/init.js
+++
@@ -1,8 +0,0 @@
-  // // LUA event listener
-  window.addEventListener('message', function(event) {
-    if (event.data.action == 'OpenMenu') {
-      $('#wrapper').show();
-    } else if (event.data.action == 'CloseMenu') {
-      $('#wrapper').hide();
-    }
-  });
